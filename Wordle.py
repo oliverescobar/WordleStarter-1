8846af8d171c3943c6a39952965ff414bccdfadc
oliverescobar <103139909+oliverescobar@users.noreply.github.com>
2024-01-22 16:23:07 -0700

    checks to see if it is a word

--- Wordle.py
+++ Wordle.py
@@ -12,30 +12,33 @@ from WordleDictionary import FIVE_LETTER_WORDS
 from WordleGraphics import WordleGWindow, N_COLS, N_ROWS
 
 def wordle():
+
     def enter_action(s):
         current_row = gw.get_current_row()
-        iCount = 0
-        next_row = 0 + iCount
-        current_word = ""
-        for col in range(N_COLS):
-            current_word += gw.get_square_letter(current_row, col)
-        if current_word in FIVE_LETTER_WORDS:
-            gw.show_message('Good job. This is a word.')
-            gw.set_current_row(next_row+1)
-            iCount = next_row+1
+        if current_row>= N_COLS:
+            gw.show_message(f'You have lost. The word was {sRandWord}')
         else:
-            gw.show_message("Not in the word list.")
-            gw.set_current_row(next_row+1)
-            iCount = next_row+1
+            next_row = 0
+            current_word = ""
+            for col in range(N_COLS):
+                current_word += gw.get_square_letter(current_row, col)
+            print(f"Word: {current_word}")
+
+            if current_word.lower() in FIVE_LETTER_WORDS:
+                gw.show_message('Good job. This is a word.')
+                next_row = current_row+1
+                gw.set_current_row(next_row)
+            else:
+                gw.show_message("Not in the word list.")
 
     gw = WordleGWindow()
     sRandWord=random.choice(FIVE_LETTER_WORDS)
     gw.add_enter_listener(enter_action)
-    for iCountRows in range(N_ROWS):
-        for iCountColumns in range(N_COLS):
-            gw.set_square_letter(iCountRows, iCountColumns, sRandWord[iCountColumns])
-            iCountColumns=iCountColumns+1
-        iCountRows=iCountRows+1
+    # for iCountRows in range(N_ROWS):
+    #     for iCountColumns in range(N_COLS):
+    #         gw.set_square_letter(iCountRows, iCountColumns, sRandWord[iCountColumns])
+    #         iCountColumns=iCountColumns+1
+    #     iCountRows=iCountRows+1
 
 # Startup code
 
