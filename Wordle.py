05752e7c951edd7fe5298aaf752ef760b1bf0311
oliverescobar <103139909+oliverescobar@users.noreply.github.com>
2024-01-22 15:10:25 -0700

    updates

--- Wordle.py
+++ Wordle.py
@@ -12,22 +12,33 @@ from WordleDictionary import FIVE_LETTER_WORDS
 from WordleGraphics import WordleGWindow, N_COLS, N_ROWS
 
 def wordle():
+    def enter_action(s):
+        current_row = gw.get_current_row()
+        iCount = 0
+        next_row = 0 + iCount
+        current_word = ""
+        for col in range(N_COLS):
+            current_word += gw.get_square_letter(current_row, col)
+        if current_word in FIVE_LETTER_WORDS:
+            gw.show_message('Good job. This is a word.')
+            gw.set_current_row(next_row+1)
+            iCount = next_row+1
+        else:
+            gw.show_message("Not in the word list.")
+            gw.set_current_row(next_row+1)
+            iCount = next_row+1
+
     gw = WordleGWindow()
     sRandWord=random.choice(FIVE_LETTER_WORDS)
+    gw.add_enter_listener(enter_action)
     for iCountRows in range(N_ROWS):
         for iCountColumns in range(N_COLS):
             gw.set_square_letter(iCountRows, iCountColumns, sRandWord[iCountColumns])
             iCountColumns=iCountColumns+1
         iCountRows=iCountRows+1
 
-
-    def enter_action(s):
-        gw.show_message("You have to implement this method.")
-
-    gw = WordleGWindow()
-    gw.add_enter_listener(enter_action)
-
 # Startup code
 
 if __name__ == "__main__":
+
     wordle()
